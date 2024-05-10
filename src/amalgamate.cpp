3b383b96e2a825b4c9f8681b54487f43291d6b72
ion098 <146852218+ion098@users.noreply.github.com>
2024-05-10 22:07:03 +0000

    added line directives to amalgamate.cpp

--- src/amalgamate.cpp
+++ src/amalgamate.cpp
@@ -1,24 +1,71 @@
+#line 1 "lemlib/chassis/motions/moveToPose.cpp"
 #include "../vsrc/lemlib/chassis/motions/moveToPose.cpp"
+
+#line 1 "lemlib/chassis/motions/moveToPoint.cpp"
 #include "../vsrc/lemlib/chassis/motions/moveToPoint.cpp"
+
+#line 1 "lemlib/chassis/motions/pursuit.cpp"
 #include "../vsrc/lemlib/chassis/motions/pursuit.cpp"
+
+#line 1 "lemlib/chassis/motions/swingToHeading.cpp"
 #include "../vsrc/lemlib/chassis/motions/swingToHeading.cpp"
+
+#line 1 "lemlib/chassis/motions/swingToPoint.cpp"
 #include "../vsrc/lemlib/chassis/motions/swingToPoint.cpp"
+
+#line 1 "lemlib/chassis/motions/turnToHeading.cpp"
 #include "../vsrc/lemlib/chassis/motions/turnToHeading.cpp"
+
+#line 1 "lemlib/chassis/motions/turnToPoint.cpp"
 #include "../vsrc/lemlib/chassis/motions/turnToPoint.cpp"
+
+#line 1 "lemlib/chassis/chassis.cpp"
 #include "../vsrc/lemlib/chassis/chassis.cpp"
+
+#line 1 "lemlib/chassis/odom.cpp"
 #include "../vsrc/lemlib/chassis/odom.cpp"
+
+#line 1 "lemlib/chassis/opcontrol.cpp"
 #include "../vsrc/lemlib/chassis/opcontrol.cpp"
+
+#line 1 "lemlib/chassis/trackingWheel.cpp"
 #include "../vsrc/lemlib/chassis/trackingWheel.cpp"
+
+#line 1 "lemlib/logger/baseSink.cpp"
 #include "../vsrc/lemlib/logger/baseSink.cpp"
+
+#line 1 "lemlib/logger/buffer.cpp"
 #include "../vsrc/lemlib/logger/buffer.cpp"
+
+#line 1 "lemlib/logger/infoSink.cpp"
 #include "../vsrc/lemlib/logger/infoSink.cpp"
+
+#line 1 "lemlib/logger/logger.cpp"
 #include "../vsrc/lemlib/logger/logger.cpp"
+
+#line 1 "lemlib/logger/message.cpp"
 #include "../vsrc/lemlib/logger/message.cpp"
+
+#line 1 "lemlib/logger/stdout.cpp"
 #include "../vsrc/lemlib/logger/stdout.cpp"
+
+#line 1 "lemlib/logger/telemetrySink.cpp"
 #include "../vsrc/lemlib/logger/telemetrySink.cpp"
+
+#line 1 "lemlib/driveCurve.cpp"
 #include "../vsrc/lemlib/driveCurve.cpp"
+
+#line 1 "lemlib/exitcondition.cpp"
 #include "../vsrc/lemlib/exitcondition.cpp"
+
+#line 1 "lemlib/pid.cpp"
 #include "../vsrc/lemlib/pid.cpp"
+
+#line 1 "lemlib/pose.cpp"
 #include "../vsrc/lemlib/pose.cpp"
+
+#line 1 "lemlib/timer.cpp"
 #include "../vsrc/lemlib/timer.cpp"
+
+#line 1 "lemlib/util.cpp"
 #include "../vsrc/lemlib/util.cpp"
